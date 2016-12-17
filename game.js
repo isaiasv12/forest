412223f23645757a056b656ac387ab26f921a086
isaias <isaias.villa@student.uncommonhs.org>
2016-12-17 21:30:36 +0000

    final commit

---
+++ game.js
@@ -0,0 +1,68 @@
+$("#a").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 7600);
+$("#h").animate({
+    top:"+=2500",
+    width:"50px"
+},7800)
+$("#r").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 8000);
+$("#g").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 8200);
+$("#s").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 8400);
+$("#t").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 8600); 
+$("#u").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 8500);    
+$("#v").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 8700);    
+$("#w").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 8900);    
+$("#x").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 9000);   
+$("#z").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 9100);
+$("#b").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 9200);
+$("#c").animate({
+    top:"+=2500",
+    width:"50px"
+    },9300);
+$("#d").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 9400);
+$("#e").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 9500);
+$("#f").animate({
+    top:"+=2500",
+    width:"50px"
+    }, 10000,function(){
+    $("#hidden").css("display","block")    
+    });    
+
+    
